b91af26b49707d9a63a8bdf4bbbbc508b82cf2f6
Namkhaidorj Batbaatar <mglcoc102@gmail.com>
2024-04-10 23:32:34 +0800

    test2

--- adminfrontend/src/instance.ts
+++ adminfrontend/src/instance.ts
@@ -1,9 +1,9 @@
 /** @format */
 
 import axios from "axios";
-
+const localBackEnd = "http://localhost:8080";
+const deployedBackEnd = "//e-commerce-e3ij.onrender.com";
 export const instance = axios.create({
-  // baseURL: "https://e-commerce-e3ij.onrender.com",
-  baseURL: "http://localhost:8080",
+  baseURL: deployedBackEnd,
   timeout: 8000,
 });
